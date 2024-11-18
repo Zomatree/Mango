da8f832df08b73b3f29a3d011ae856229eb22df8
Zomatree <me@zomatree.live>
2024-11-18 15:16:21 +0000

    Initial Commit

---
+++ MangaApp/MangaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaAppApp.swift
+//  MangaApp
+//
+//  Created by Angelo on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MangaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
